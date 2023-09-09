0ee1ae3c221a7ba2f0faaeacae8d9120284f5295
DeeMMoon <37477667+DeeMMoon@users.noreply.github.com>
2023-09-09 22:39:32 +0300

    Update main.c

--- src/server/main.c
+++ src/server/main.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 int main() {
-while (FCGI_Accept() >= 0)
-printf("Content-type: text/html\n"
-"\n"
-"<title>Hello World!</title>"
-"<h1>Hello World!</h1>\n");
+  while (FCGI_Accept() >= 0)
+    printf("Content-type: text/html\n"
+    "\n"
+    "<title>Hello World!</title>"
+    "<h1>Hello World!</h1>\n");
 return 0;
 }
